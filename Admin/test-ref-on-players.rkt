1b3ca347943c770c47d88e78183668c6de169b42
Matthias Felleisen <matthias@ccs.neu.edu>
2021-08-10 18:58:08 -0400

    turn into stress test

--- Admin/test-ref-on-players.rkt
+++ Admin/test-ref-on-players.rkt
@@ -3,7 +3,10 @@
 (require Trains/Admin/referee)
 (require Trains/Player/player)
 (require Trains/Player/simple-strategy)
+(require (submod Trains/Editor/map-editor homework))
+(require Trains/Common/map)
 (require (submod Trains/Common/map examples))
+(require (prefix-in im: 2htdp/image))
 
 #; {Ranking -> Void}
 (define (display-results results)
@@ -20,9 +23,26 @@
     [else (~a i "th")]))
 
 
-(define p1 (new player% [strategy% simple-strategy%]))
-(define p2 (new player% [strategy% simple-strategy%]))
+(define (make-players i)
+  (build-list i (λ (i) (new player% [strategy% simple-strategy%] [name (~a "player" i)]))))
 
-(define results (referee (list p1 p2) vrectangle))
+(define the-map (construct-random-map (build-list 20 (compose string->symbol ~a)) 40 200 800))
+
+
+#; {[Listof InternalConnection] [List String [List N N]] Image -> Image}
+draw-connections
+   
+#; {[List String [List N N]] Image -> Image}
+(define (external->internal-connections externals)
+  (for/list ([c (in-set externals)])
+    (list* (~a (connection-from c)) (~a (connection-to c)) (cddr c))))
+
+(define cities (map (lambda (x) (cons (~a (first x)) (rest x))) (game-map-locations the-map)))
+(define conns  (external->internal-connections (game-map-all-connections the-map))) 
+(define +cities (draw-cities cities (im:empty-scene 200 800)))
+(draw-connections conns cities +cities)
+
+
+(define results (referee (make-players 8) the-map))
 
 (display-results results)
