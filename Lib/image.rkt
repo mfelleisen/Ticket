257715d2560bdc65f91f55a8e261de2f58871790
Matthias Felleisen <matthias@ccs.neu.edu>
2021-08-23 18:33:07 -0400

    use somewhat thicker pen for connections

--- Lib/image.rkt
+++ Lib/image.rkt
@@ -15,28 +15,35 @@
 
 ;; -----------------------------------------------------------------------------
 (define (add-segments background n x1 y1 x2 y2 line-color background-color)
-  (define s (add-line background x1 y1 x2 y2 line-color))
+  (define thickness 3)
+  (define radius (* .9 thickness))
+  (define p (make-pen line-color thickness "solid" "projecting" "round"))
+  (define s (add-line background x1 y1 x2 y2 p #;line-color))
   (define delta-x (quotient (- x2 x1) n))
   (define delta-y (quotient (- y2 y1) n))
   (for/fold ([s s]) ([i (in-range 1 n 1)])
     (define x-mid (+ x1 (* i delta-x)))
     (define y-mid (+ y1 (* i delta-y)))
-    (place-image (circle 3 'solid background-color) x-mid y-mid s)))
+    (place-image (circle radius 'solid background-color) x-mid y-mid s)))
 
 ;; -----------------------------------------------------------------------------
 ;; this isn't really a test 
 (module+ picts
   (define WIDTH 200)
   (define HEIGHT 300)
+  (define COLOR 'blue)
 
   (define A (list (random WIDTH) (random HEIGHT)))
   (define B (list (random WIDTH) (random HEIGHT)))
+  (define S (+ 3 (random 3)))
+
+  (list A B S)
 
   (define BACK
-    (let* ([s (rectangle WIDTH HEIGHT 'solid 'white)]
+    (let* ([s (rectangle WIDTH HEIGHT 'solid COLOR)]
            [s (place-image (text "A" 11 'black) (first A) (second A) s)]
            [s (place-image (circle 5 'solid 'red) (first A) (second A) s)]
            [s (place-image (text "B" 11 'black) (first B) (second B) s)]
            [s (place-image (circle 5 'solid 'red) (first B) (second B) s)])
       s))
-  (add-segments BACK 5 (first A) (second A) (first B) (second B) 'blue 'white))
+  (add-segments BACK S (first A) (second A) (first B) (second B) 'white  COLOR))
