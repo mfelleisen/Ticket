c76c8b781e3955b18f33e9b3bc7cc58bdf6034de
Matthias Felleisen <matthias@ccs.neu.edu>
2021-09-02 17:27:36 -0400

    remote dispatcher and manager

---
+++ Remote/define-dispatcher.rkt
@@ -0,0 +1,83 @@
+#lang racket
+
+;; create a dispatcher from types 
+
+(provide
+ define-dispatcher)
+
+;; ---------------------------------------------------------------------------------------------------
+(require Trains/Common/json)
+(require (for-syntax syntax/parse))
+(require (for-syntax (only-in racket ~a)))
+
+;; ---------------------------------------------------------------------------------------------------
+(begin-for-syntax
+  (define-syntax-class dispatcher-clause
+    (pattern [(method:id arg:id ...) return:id])))
+
+(define-syntax (define-dispatcher stx)
+  (syntax-parse stx
+    [[_ dispatcher c:dispatcher-clause ...]
+     #:with player #'player
+     #:with ((pattern right-hand-side) ...) (map (match-clause #'player) (syntax->list #'[ c ...]))
+     #:with (pat1 pat ... patN) #'(pattern ...)
+     #:with (rhs1 rhs ... rhsN) #'(right-hand-side ...)
+     #`(define ([dispatcher player done?] x)
+         (match x
+           [(? eof-object?) #false]
+           [pat1 (set-box! done? 'go) rhs1]
+           [pat  rhs]
+           ...
+           [patN (set-box! done? #t) rhsN]
+           [ill (error 'dispatcher "server sent ill-formed message: ~e" ill)]))]))
+
+(define-for-syntax ((match-clause player) stx)
+  (syntax-parse stx
+    [[(method:id arg:id ...) return:id]
+     #:with (names pat) (create-pattern #'(method arg ...))
+     #:with right-hand-side (create-rhs player #'return #'method #'names)
+     (list #'pat #'right-hand-side)]))
+
+(define-for-syntax (create-pattern stx)
+  (syntax-parse stx
+    [(method:id arg:id ...)
+     #:with method-str    (symbol->string (syntax-e #'method))
+     #:with (x ...)       (generate-temporaries #'(arg ...))
+     #:with (arg? ...)    (for/list ([a (syntax->list #'(arg ...))]) (predicate-name a))
+     #:with (p-arg ...)   (for/list ([a (syntax->list #'(arg ...))]) (parse-name a))
+     #:with (arg-pat ...) #'[(app p-arg (? arg? x)) ...]
+     (list #'(x ...) #'`[method-str [,arg-pat ...]])]))
+
+(define-for-syntax (create-rhs player return method names)
+  #`(#,(jsexpr-name return) (send #,player #,method #,@names)))
+
+(define-for-syntax (parse-name id)
+  (datum->syntax id (string->symbol (~a "parse-" (syntax-e id))) id id))
+
+(define-for-syntax (predicate-name id)
+  (datum->syntax id (string->symbol (~a (syntax-e id) "?")) id id))
+
+(define-for-syntax (jsexpr-name id)
+  (datum->syntax id (string->symbol (~a (syntax-e id) "->jsexpr")) id id))
+
+;; ---------------------------------------------------------------------------------------------------
+(module+ test
+  (define (parse-symbol x) (string->symbol x))
+  
+  (define player%
+    (class object%
+      (super-new)
+      (define/public (pick x) (displayln `[I picked ,x]))
+      (define/public (done x) (displayln `[I am done: ,x]) "I won")))
+ 
+  (define-dispatcher dispatcher 
+    [(pick symbol) void]
+    [(done boolean) string])
+
+  (define player (new player%))
+
+  ([dispatcher player (box 1)] '["pick" ["a"]])
+  ([dispatcher player (box 1)] '["done" [#false]])
+  (with-handlers ([exn:fail? (λ (xn) (void))])
+    [(dispatcher player (box 0)) '["done" []]]
+    (raise 'ouch)))
---
+++ Remote/manager.rkt
@@ -0,0 +1,164 @@
+#lang racket
+
+;; a remote manager that connects a single player to a client system, which connects to a server 
+
+;                                                          
+;                                                          
+;                                                          
+;                                             ;            
+;                                             ;            
+;    ;;;;   ;   ;;  ; ;;;    ;;;;    ;;;;;  ;;;;;;   ;;;;  
+;    ;  ;;   ;  ;   ;;  ;   ;;  ;;   ;;       ;     ;    ; 
+;   ;    ;    ;;    ;    ;  ;    ;   ;        ;     ;      
+;   ;;;;;;    ;;    ;    ;  ;    ;   ;        ;     ;;;    
+;   ;         ;;    ;    ;  ;    ;   ;        ;        ;;; 
+;   ;         ;;    ;    ;  ;    ;   ;        ;          ; 
+;    ;       ;  ;   ;;  ;   ;;  ;;   ;        ;     ;    ; 
+;    ;;;;;  ;    ;  ; ;;;    ;;;;    ;         ;;;   ;;;;  
+;                   ;                                      
+;                   ;                                      
+;                   ;                                      
+;                                                          
+
+(require (only-in Trains/Common/player-interface manager-player/c))
+(require (only-in json jsexpr?))
+
+(define receiver/c (-> (-> (or/c eof-object? jsexpr?) jsexpr?) any))
+
+(provide
+ (contract-out
+  [make-remote-manager (-> receiver/c (-> manager-player/c any))]))
+
+;                                                                                      
+;       ;                                  ;                                           
+;       ;                                  ;                          ;                
+;       ;                                  ;                                           
+;    ;;;;   ;;;   ;;;;    ;;;   ; ;;    ;;;;   ;;;   ; ;;    ;;;    ;;;    ;;;    ;;;  
+;   ;; ;;  ;;  ;  ;; ;;  ;;  ;  ;;  ;  ;; ;;  ;;  ;  ;;  ;  ;;  ;     ;   ;;  ;  ;   ; 
+;   ;   ;  ;   ;; ;   ;  ;   ;; ;   ;  ;   ;  ;   ;; ;   ;  ;         ;   ;   ;; ;     
+;   ;   ;  ;;;;;; ;   ;  ;;;;;; ;   ;  ;   ;  ;;;;;; ;   ;  ;         ;   ;;;;;;  ;;;  
+;   ;   ;  ;      ;   ;  ;      ;   ;  ;   ;  ;      ;   ;  ;         ;   ;          ; 
+;   ;; ;;  ;      ;; ;;  ;      ;   ;  ;; ;;  ;      ;   ;  ;;        ;   ;      ;   ; 
+;    ;;;;   ;;;;  ;;;;    ;;;;  ;   ;   ;;;;   ;;;;  ;   ;   ;;;;   ;;;;;  ;;;;   ;;;  
+;                 ;                                                                    
+;                 ;                                                                    
+;                 ;                                                                    
+
+(require Trains/Remote/define-dispatcher)
+(require Trains/Common/json)
+(require Trains/Common/basic-constants-serialize)
+(require Trains/Common/map-serialize)
+(require (except-in Trains/Common/player-interface manager-player/c))
+(require Trains/Common/state-serialize)
+(require Trains/Common/state)
+(require Trains/Common/map)
+(require SwDev/Testing/communication)
+
+(module+ test
+  (require (submod ".."))
+  (require (submod Trains/Common/map examples))
+  (require (submod Trains/Common/map-serialize examples))
+  (require (submod Trains/Common/state-serialize examples))
+  (require Trains/Common/state)
+  (require Trains/Player/player)
+  (require Trains/Player/hold-10-strategy)
+  (require SwDev/Lib/should-be-racket)
+  (require rackunit))
+
+;                                                                               
+;                                                        ;                      
+;                                                        ;            ;         
+;                                                        ;                      
+;   ;;;;    ;;;;   ;;;   ;   ;  ;   ;         ;;;;    ;;;; ;;;;;;   ;;;   ; ;;  
+;   ;; ;;   ;;  ; ;; ;;   ; ;   ;   ;             ;  ;; ;; ;  ;  ;    ;   ;;  ; 
+;   ;   ;   ;     ;   ;   ;;;    ; ;              ;  ;   ; ;  ;  ;    ;   ;   ; 
+;   ;   ;   ;     ;   ;    ;     ; ;           ;;;;  ;   ; ;  ;  ;    ;   ;   ; 
+;   ;   ;   ;     ;   ;   ;;;    ; ;          ;   ;  ;   ; ;  ;  ;    ;   ;   ; 
+;   ;; ;;   ;     ;; ;;   ; ;    ;;           ;   ;  ;; ;; ;  ;  ;    ;   ;   ; 
+;   ;;;;    ;      ;;;   ;   ;    ;            ;;;;   ;;;; ;  ;  ;  ;;;;; ;   ; 
+;   ;                             ;                                             
+;   ;                            ;                                              
+;   ;                           ;;
+
+
+(define ((make-remote-manager receiver) player)
+  (define done? (box (gensym)))
+  (define disp  (dispatcher player done?))
+  (parameterize ([io-time-out 1000])
+    (let loop ()
+      (with-handlers ([void (λ (xn)
+                              (fprintf (current-error-port) "~a" (exn-message xn))
+                              (set-box! done? xn))])
+        (receiver disp)
+        (unless (boolean? (unbox done?))
+          (loop)))))
+  (unbox done?))
+
+#;{XPlayer [Box (U Symbol Boolean)] -> JSexpr -> JSexpr}
+(define-dispatcher dispatcher
+  [[start boolean] game-map]
+  [[setup game-map natural color*] void]
+  [[pick  destination-set] destination-set]
+  [[play  pstate] action]
+  [[more  color*] void]
+  [[win   boolean] void]
+  [[end   boolean] void])
+
+;                                     
+;                                     
+;     ;                    ;          
+;     ;                    ;          
+;   ;;;;;   ;;;    ;;;   ;;;;;   ;;;  
+;     ;    ;;  ;  ;   ;    ;    ;   ; 
+;     ;    ;   ;; ;        ;    ;     
+;     ;    ;;;;;;  ;;;     ;     ;;;  
+;     ;    ;          ;    ;        ; 
+;     ;    ;      ;   ;    ;    ;   ; 
+;     ;;;   ;;;;   ;;;     ;;;   ;;;  
+;                                     
+;                                     
+;                                     
+
+(module+ test
+  (define player1 (new player% [the-map vtriangle] [strategy% hold-10-strategy%]))
+  (define box1    (box (gensym)))
+
+  (check-false [(dispatcher player1 box1) eof])
+
+  (check-equal? [(dispatcher player1 box1) `["start" [#true]]] vtriangle-serialized)
+  (check-true (symbol? (unbox box1)))
+
+  (check-equal? [(dispatcher player1 box1) `["setup" [,vtriangle-serialized 4 ["red" "red"]]]] "void")
+  (check-true (symbol? (unbox box1)))
+
+  (define (dests n [convert (λ (a b) (list (~a a) (~a b)))])
+    (define targets
+      (build-list n (λ (i) (string->symbol (~a "Seat" (integer->char (+ (char->integer #\a) i)))))))
+    (map convert (make-list n 'Boston) targets))
+  (check-equal? [(dispatcher player1 box1) `["pick" [,(dests 5)]]] (drop (dests 5) 2))
+  (check-true (symbol? (unbox box1)))
+
+  (check-equal? [(dispatcher player1 box1) `["play" [,pstate1-serialized]]] MORE)
+  (check-true (symbol? (unbox box1)))
+
+  (check-equal? [(dispatcher player1 box1) `["more" [["blue" "blue" "red"]]]] "void" "tecn. Illegale")
+  (check-true (symbol? (unbox box1)))
+
+  (check-equal? [(dispatcher player1 box1) `["win" [#true]]] "void")
+  (check-true (symbol? (unbox box1)))
+
+  (check-equal? [(dispatcher player1 box1) `["end" [#false]]] "void")
+  (check-true (boolean? (unbox box1)))
+
+  (check-true ((make-remote-manager (λ (f) (f `["end" [#true]]))) player1))
+  
+  (define b*
+    `[["start" [#true]]
+      ["setup" [,vtriangle-serialized 3 ["red" "red"]]]
+      ["pick" [,(dests 5)]]
+      ["play" [,pstate1-serialized]]
+      ["win" [#true]]
+      ["end" [#true]]])
+  (check-true ((make-remote-manager (λ (f) (begin0 (f (first b*)) (set! b* (rest b*))))) player1))
+ 
+  (check-pred exn? (dev/null ((make-remote-manager (λ (f) (f `[0 [#true]]))) player1))))
