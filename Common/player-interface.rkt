0ea95a504a67391e95f43d8f6d5869213c65eb2a
Matthias Felleisen <matthias@ccs.neu.edu>
2021-10-11 12:15:00 -0400

    protocol for referee-player, inlined; see Common/ diagrams

--- Common/player-interface.rkt
+++ Common/player-interface.rkt
@@ -75,12 +75,26 @@
 
 (define referee-player%/c
   (class/c
-   [init-field [strategy% strategy%/c] [name city?]]
+   ;; hand the player the map for the game, a number of rails, and some cards
    [setup (->m game-map? natural? (listof color?) any/c)]
+
+   ;; ask the player to pick some destinations and to return the remainder 
    [pick  (->m (set/c destination/c) (set/c destination/c))]
+
+   ;; grant the player the right to take a turn 
    [play  (->m pstate? (or/c MORE action?))]
+
+   ;; if the preceding call to `play` returned `MORE`, call `more` to hand out more cards
    [more  (->m (listof color?) any/c)]
+
+   ;; inform the player whether it won (#t)/lost (#f) the game 
    [win   (->m boolean? any/c)]))
+
+;; A proper game interaction sequence is a word in this regular expression:
+;;    setup, pick, {play | more}*, win
+;; meaning the referee calls the player with setup, followed by one call to pick,
+;; followed by an arbitrary number of calls to `play` and possibly `more`, with
+;; one final call to `win` at the very end of a game. 
 
 (define manager-player%/c
   (class/c
