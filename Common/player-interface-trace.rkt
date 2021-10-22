b2a8fdec3ce6660827517bd1872648ae67259a90
Matthias Felleisen <matthias@ccs.neu.edu>
2021-10-22 09:29:58 -0400

    positive naming

--- Common/player-interface-trace.rkt
+++ Common/player-interface-trace.rkt
@@ -49,6 +49,8 @@
 (require Trains/Common/map)
 (require Trains/Common/state)
 
+(require trace-contract)
+
 ;                                                                          
 ;                                                                          
 ;      ;                                      ;                            
@@ -67,34 +69,81 @@
 ;                                                                          
 
 (define MORE "more cards")
-
 (define action? connection/c)
 
-(define strategy%/c
-  (class/c))
+(define-syntax-rule (tag-trace t) (list/t 't t))
 
 (define referee-player%/c
-  (class/c
-   ;; hand the player the map for the game, a number of rails, and some cards
-   [setup (->m game-map? natural? (listof color?) any/c)]
+  (trace/c ([setup any/c]
+            [pick  set?]
+            [play  (or/c action? MORE)]
+            [more  (listof color?)]
+            [win   any/c])
+    
+    (class/c
+     [setup
+      ;; hand the player the map for the game, a number of rails, and some cards
+      (->m game-map? natural? (listof color?) (tag-trace setup))]
+     
+     [pick
+      ;; ask the player to pick some destinations and to return the remainder 
+      (->m (set/c destination/c) (tag-trace pick))]
+     
+     [play
+      ;; grant the player the right to take a turn 
+      (->m pstate? (tag-trace play))]
 
-   ;; ask the player to pick some destinations and to return the remainder 
-   [pick  (->m (set/c destination/c) (set/c destination/c))]
+     [more
+      ;; if the preceding call to `play` returned `MORE`, call `more` to hand out more cards
+      (->m (listof color?) (tag-trace more))]
+     
+     [win
+      ;; inform the player whether it won (#t)/lost (#f) the game 
+      (->m boolean? (tag-trace win))])
+    
+    ;; A proper game interaction sequence is a word in this regular expression:
+    ;;    setup, pick, {play | more}*, win
+    ;; meaning the referee calls the player with setup, followed by one call to pick,
+    ;; followed by an arbitrary number of calls to `play` and possibly `more`, with
+    ;; one final call to `win` at the very end of a game.
+    ((setup pick play more win) (proper-call-order? (trace-merge setup pick play more win))
+                                ;; is this necessary? 
+                                #:recover (λ _ '()))))
 
-   ;; grant the player the right to take a turn 
-   [play  (->m pstate? (or/c MORE action?))]
+(define PPM `[pick play more])
 
-   ;; if the preceding call to `play` returned `MORE`, call `more` to hand out more cards
-   [more  (->m (listof color?) any/c)]
+;; checks the protocol specified above 
+(define (proper-call-order? trace-0)
+  (define starter (first (stream-first trace-0)))
+  (and
+   (equal? 'setup starter)
+   (let L ([t (stream-rest trace-0)] [last-seen starter])
 
-   ;; inform the player whether it won (#t)/lost (#f) the game 
-   [win   (->m boolean? any/c)]))
+     ;; for proper debugging, I needed this:
+     (define-syntax-rule (-> lst then)
+       (if (member last-seen lst) then (show-error last-seen lst trace-0 t)))
 
-;; A proper game interaction sequence is a word in this regular expression:
-;;    setup, pick, {play | more}*, win
-;; meaning the referee calls the player with setup, followed by one call to pick,
-;; followed by an arbitrary number of calls to `play` and possibly `more`, with
-;; one final call to `win` at the very end of a game. 
+     (cond
+       [(stream-empty? t) #true]
+       [else (match-define `[,one ,two] (stream-first t))
+             (define others (stream-rest t))
+             (case one
+               [(setup) #false]
+               [(pick)  (-> '[setup] (L others 'pick))]
+               [(play)  (-> PPM      (L others (if (equal? MORE two) 'more one)))]
+               [(more)  (-> `[more]  (L others 'more))]
+               [(win)   (-> PPM      (stream-empty? others))]
+               [else    #false])]))))
+
+;; debugging aid
+(define (show-error last-seen lst trace0 t)
+  (displayln `[last-seen is ,last-seen lst is ,lst] (current-error-port))
+  (displayln `[trace0 is ,(for/list ([x (in-stream trace0)]) x)] (current-error-port))
+  (displayln `[failure at ,(for/list ([x (in-stream t)]) x)] (current-error-port))
+  #false)
+
+;; ---------------------------------------------------------------------------------------------------
+#; {Stream : not enpty -> Boolean}
 
 (define manager-player%/c
   (class/c
